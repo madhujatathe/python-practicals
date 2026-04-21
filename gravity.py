abb87ffccb68890c90ab06634c0111f8c6f43066
madhujatathe <madhutathe38@gmail.com>
2026-04-21 18:04:58 +0530

    Update and rename madhujacode3.py to gravity.py

---
+++ gravity.py
@@ -0,0 +1,8 @@
+#on a certain planet a correctly calibrated spring balance shows the weigth of a body 12N, the mass of which is 4.893kg. write a program to find the value of gravity on this planet 
+W = 12
+m = 4.893
+g = W / m
+
+print(f"Weight (W) = {W} N")
+print(f"Mass (m) = {m} kg")		
+print(f"Gravity (g) = {g} m/s^2")
