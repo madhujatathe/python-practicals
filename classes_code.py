15df1d9c092d5bd0f9210b2832065dc0984094b9
madhujatathe <madhutathe38@gmail.com>
2026-04-23 22:33:48 +0530

    Created classes_code.py

---
+++ classes_code.py
@@ -0,0 +1,36 @@
+class Car :
+    speed=0
+    milage=0
+    average=0
+
+    def run(self):
+        print("car is runing")
+
+    
+c = Car()
+
+c.speed = input("Enter the speed value: ")
+
+c.milage = input("enter the milage: ")
+
+c.average = input("enter the avrage: ")
+
+
+print(c.speed)
+print(c.milage)
+print(c.average)
+
+# c.run()
+
+c2 = Car()
+
+c2.speed = input("Enter the speed value: ")
+
+c2.milage = input("enter the milage: ")
+
+c2.average = input("enter the avrage: ")
+
+
+print(c2.speed)
+print(c2.milage)
+print(c2.average)
